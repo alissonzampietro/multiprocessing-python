6a2ee38f26ca1302bae101fd763a4b640012485e
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-07-29 01:39:56 +0100

    feat: creating sleep methods and counting

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('bem vindo')
+import time
+
+start = time.perf_counter()
+
+def sleep():
+    print('Sleeping 1 second...')
+    time.sleep(1)
+    print('Waking up dude...')
+
+sleep();
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
